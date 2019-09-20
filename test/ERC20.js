2fa24f597ed4b9f28c47e566fb4849315bb70b96
Role Engineer <roleengineer@gmail.com>
2019-09-20 06:51:23 +0300

    added unit test

--- test/ERC20.js
+++ test/ERC20.js
@@ -4,6 +4,7 @@ const SmtLib = require('../SmtLib.js');
 contract("ERC20", (accounts) => {
   const tokens10k = "0x0000000000000000000000000000000000000000000000000000000000002710";
   const tokens9k  = "0x0000000000000000000000000000000000000000000000000000000000002328";
+  const tokens8k  = "0x0000000000000000000000000000000000000000000000000000000000001f40";
   const tokens1k  = "0x00000000000000000000000000000000000000000000000000000000000003e8";
   const tokens750 = "0x00000000000000000000000000000000000000000000000000000000000002ee";
   const tokens250 = "0x00000000000000000000000000000000000000000000000000000000000000fa";
@@ -209,5 +210,62 @@ contract("ERC20", (accounts) => {
     assert.equal(rsp, true, "Must return true");
     rsp = await erc20.balanceOf(accounts[3], 250, tree.createMerkleProof(accounts[3]));
     assert.equal(rsp, true, "Must return true");
+  });
+
+  it("should send tokens correctly when sender and recipient is the same address", async () => {
+    const erc20 = await ERC20.deployed();
+
+    assert.equal(leaves[accounts[0]], tokens9k, 'Wrong initial state');
+    //Demonstrates that the proof for one key(address) isn't changed when you changed the leaf (proof is changed when other leaves are changed).
+    let sender_proof = tree.createMerkleProof(accounts[0]);
+
+    leaves[accounts[0]] = tokens8k;
+    let tree_transitional = new SmtLib(160, leaves);
+    assert.equal(sender_proof, tree_transitional.createMerkleProof(accounts[0]), 'Wrong initial state');
+
+
+    //Negative call (using output parameters that aws service give)
+    try {
+      let res = await erc20.transfer.call(9000, sender_proof, accounts[0], 9000, tree_transitional.createMerkleProof(accounts[0]), 1000, { from: accounts[0]});
+    } catch (e) {
+      assert(e.message.indexOf("revert") >= 0, "error message must contain revert");
+    }
+
+    //Send transaction (using output parameters that aws service give)- transfers ownership of 1000 tokens from first account to first account
+    try {
+      let tx = await erc20.transfer(9000, sender_proof, accounts[0], 9000, tree_transitional.createMerkleProof(accounts[0]), 1000, { from: accounts[0]});
+    } catch (e) {
+      assert(e.message.indexOf("revert") >= 0, "error message must contain revert");
+      assert.equal(e.reason, 'update proof not valid', "incorrect reason of the revert")
+    }
+
+    //Correct call. Remark: sender and recipient is the same address, but the balances are different, because of the writting order. When transfer happens firstly rewrites senders balance, so before rewriting recipient balance happens the tree consist the new value of the recipient balance, this value we should put as a parameter like we do with proof.
+    let res = await erc20.transfer.call(9000, sender_proof, accounts[0], 8000, tree_transitional.createMerkleProof(accounts[0]), 1000, { from: accounts[0]});
+    assert.equal(res, true, "Must return true");
+
+    //Send transaction - transfers ownership of 1000 tokens from first account to first account
+    let tx = await erc20.transfer(9000, sender_proof, accounts[0], 8000, tree_transitional.createMerkleProof(accounts[0]), 1000, { from: accounts[0]});
+    //Check the logs
+    assert.equal(tx.logs.length, 3, 'trigger three events');
+    assert.equal(tx.logs[0].event, 'Write', 'should be the "Write" event');
+    assert.equal(tx.logs[0].args._address, accounts[0], 'logs the senders key, path in sparse merkle tree');
+    assert.equal(tx.logs[0].args._value, tokens8k, 'logs the senders value, leaf in sparse merkle tree');
+    assert.equal(tx.logs[1].event, 'Write', 'should be the "Write" event');
+    assert.equal(tx.logs[1].args._address, accounts[0], 'logs the recipients key, path in sparse merkle tree');
+    assert.equal(tx.logs[1].args._value, tokens9k, 'logs the recipients value, leaf in sparse merkle tree');
+    assert.equal(tx.logs[2].event, 'Transfer', 'should be the "Transfer" event');
+    assert.equal(tx.logs[2].args.from, accounts[0], 'logs the account the tokens are transfered from');
+    assert.equal(tx.logs[2].args.to, accounts[0], 'logs the account the tokens are transfered to');
+    assert.equal(tx.logs[2].args.value.toNumber(), 1000, 'logs the transfer amount');
+
+    //Check the balances
+    leaves[accounts[0]] = tokens9k;
+    tree = new SmtLib(160, leaves);
+
+    let rsp = await erc20.balanceOf(accounts[0], 9000, tree_transitional.createMerkleProof(accounts[0]));
+    assert(rsp, "Senders or/and recipients balance shouldn't change");
+
+    rsp = await erc20.balanceOf(accounts[0], 9000, tree.createMerkleProof(accounts[0]));
+    assert(rsp, "Senders or/and recipients balance shouldn't change");
   });
 });
