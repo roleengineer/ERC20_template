43e6037fbf589f0c0fb42cc6a4d0ee9dda92950e
Role Engineer <roleengineer@gmail.com>
2019-09-24 14:59:36 +0300

    missed extra stuff

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -32,9 +32,4 @@ interface IERC20 {
      */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /**
-     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
-     * a call to {approve}. `value` is the new allowance.
-     */
-    event Approval(address indexed owner, address indexed spender, uint256 value);
 }
